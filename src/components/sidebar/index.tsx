c8eccd70e339014365a23d20107432ffaa0f87bc
Aulia Harvy <auliaharvy@gmail.com>
2025-10-03 10:06:14 +0700

    easy tax init

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -23,7 +23,7 @@ function SidebarHorizon(props: { routes: IRoute[]; [x: string]: any }) {
 
       <div className={`mx-[56px] mt-[50px] flex items-center`}>
         <div className="ml-1 mt-1 h-2.5 font-poppins text-[26px] font-bold uppercase text-navy-700 dark:text-white">
-          Horizon <span className="font-medium">FREE</span>
+          Easy <span className="font-medium">Tax</span>
         </div>
       </div>
       <div className="mb-7 mt-[58px] h-px bg-gray-300 dark:bg-white/30" />
@@ -34,9 +34,9 @@ function SidebarHorizon(props: { routes: IRoute[]; [x: string]: any }) {
       </ul>
 
       {/* Free Horizon Card */}
-      <div className="flex justify-center">
+      {/* <div className="flex justify-center">
         <SidebarCard />
-      </div>
+      </div> */}
 
       {/* Nav item end */}
     </div>
