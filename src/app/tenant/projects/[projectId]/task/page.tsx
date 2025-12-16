a013741db7e9b2b41652cd875d1970a0cf0b37e7
ahjaradithya <ahzathenation@gmail.com>
2025-12-16 13:28:05 +0700

    fixing bug merge

--- src/app/tenant/projects/[projectId]/task/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function ProjectTaskAlias({ params }: { params: { projectId: string } }) {
-  const projectId = params.projectId;
-  redirect(`/tenant/projects/${projectId}/tasks`);
-}
