333b4d3c350ce4c1ce7dbeb8d9aef1e0b8c4b0b9
ahjaradithya <ahzathenation@gmail.com>
2025-12-22 06:16:02 +0700

    placeholder tab for spt

---
+++ src/app/tenant/projects/[projectId]/kk3/spt-management/page.tsx
@@ -0,0 +1,10 @@
+import { Card } from "@/components/ui/card";
+
+export default async function sptPage(){
+  return (
+    <Card className="w-full h-full text-center">
+        <h1>SPT Management</h1>
+        <p>Manage your SPT here.</p>
+    </Card>
+  );
+}
